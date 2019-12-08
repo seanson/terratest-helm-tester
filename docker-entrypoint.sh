0a922e257be8f7a5dcf8d8ffdcbfd807a2793e05
seanson@users.noreply.github.com <seanson@users.noreply.github.com>
2019-12-08 18:14:00 +1100

    Fixed certificate template, removed pipefail as we use pipes now.

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
-set -euo pipefail
-
-find .
 
 echo "Running tests against $1"
 
 # Run this first so we don't get fetching errors
 go test -i $1
-go test -count=1 -v "$1" | terratest_log_parser
+go test -count=1 -v "$1" | tee output.log
+terratest_log_parser --testlog output.log
