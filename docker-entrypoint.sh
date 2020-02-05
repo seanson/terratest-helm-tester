320637fdf92749f61210573725a56bf55f2ab82b
seanson@users.noreply.github.com <seanson@users.noreply.github.com>
2020-02-05 12:00:07 +1100

    chore: Cleanup noisy terratest parser output

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -8,8 +8,9 @@ go test -i $@
 go test -count=1 -v $@  | tee output.log
 EXIT_CODE=${PIPESTATUS[0]}
 
-cat output.log
-terratest_log_parser --testlog output.log
+echo "Parsing logs and writing test output to ./out"
+
+terratest_log_parser --testlog output.log >/dev/null 2>&1
 
 if [ ! -z "${USER_ID}" ]; then
     echo "USER_ID set, changing ownership of out directory."
