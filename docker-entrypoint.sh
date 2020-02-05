692f4047a8d33974f3571f05c2783bc26ed7f39b
seanson@users.noreply.github.com <seanson@users.noreply.github.com>
2020-02-05 11:52:42 +1100

    chore: Switch to bash as we have it anyways and restore tee

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,12 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 echo "Running tests with args: $@"
 
 # Run this first so we don't get fetching errors
 go test -i $@
 
-go test -count=1 -v $@  > output.log
-EXIT_CODE=$?
+go test -count=1 -v $@  | tee output.log
+EXIT_CODE=${PIPESTATUS[0]}
 
 cat output.log
 terratest_log_parser --testlog output.log
